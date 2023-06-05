fb1fae5625f9ad4c39c01e69968b8eba3cc0f461
Pious Alpha <102190049+Piouscode@users.noreply.github.com>
2023-06-05 07:04:02 +0200

    initial commit

---
+++ src/main/java/hospital/cli/HospitalcliApplication.java
@@ -0,0 +1,13 @@
+package hospital.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalcliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalcliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hospital/cli/HospitalcliApplicationTests.java
@@ -0,0 +1,13 @@
+package hospital.cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalcliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
